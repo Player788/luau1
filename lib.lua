49ab818b315e83064f178763277f5296c8752275
Player788 <110638503+Player788@users.noreply.github.com>
2022-12-03 17:20:08 +0500

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1,22 +1,12 @@
 local Library = {}
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
-
-function getId()
-	local str = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
-	local toReturn = ""
-	for i, v in pairs(str) do
-		local addNew = math.random(1, #str)
-		addNew = str[addNew]
-		toReturn = toReturn .. addNew
-	end
-	return toReturn
-end
+local UserInputService = game:GetService("UserInputService")
 
 function Library.Create(Class, Parent, Table)
 	local newObj = Instance.new(Class)
 	newObj.Parent = Parent
-	newObj.Name = getId()
+	newObj.Name = Table.Name or newObj.Name
 	for i, v in pairs(Table) do
 		local success, err = pcall(function()
 			return newObj[i]
@@ -24,7 +14,6 @@ function Library.Create(Class, Parent, Table)
 		if (success) then
 			newObj[i] = v
 		else
-			--warn(err)
 		end
 	end
 	return newObj
@@ -60,13 +49,101 @@ Library.Date = function(Pattern)
 end
 
 Library.Tween = function(GuiObj, Property, Value, Direction, Style, Duration)
-	-- implement TweenService
-	Direction = Direction or "InOut"
-	Style = Style or "Linear"
-	Duration = Duration or 0.1
+	Direction = Direction or "Out"
+	Style = Style or "Quint"
+	Duration = Duration or 0.3
 	local nTweenInfo = TweenInfo.new(Duration, Enum.EasingStyle[Style], Enum.EasingDirection[Direction])
 	local Tween = TweenService:Create(GuiObj, nTweenInfo, {[Property] = Value})
 	Tween:Play()
+	return Tween
+end
+
+Library.MouseHover = function(GuiObj, Property, Value1, Value2)
+	GuiObj.MouseEnter:Connect(function()
+		Library.Tween(GuiObj, Property, Value1, "Out", "Quint", 0.1)
+	end)
+	GuiObj.MouseLeave:Connect(function()
+		Library.Tween(GuiObj, Property, Value2, "Out", "Quint", 0.1)
+	end)
+end
+
+Library.BoolToString = function(Bool)
+	if Bool == true then
+		return "true"
+	elseif Bool == false then
+		return "false"
+	end
+end
+
+Library.toJSON = function(Table)
+	pcall(function()
+		local JSON = game:GetService("HttpService"):JSONEncode( {Table} )
+		return JSON
+	end)
+	return "Failed pcall ?"
+end
+
+Library.toTable = function(JSON)
+	pcall(function()
+		local Table = game:GetService("HttpService"):JSONDecode( {JSON} )
+		return Table
+	end)
+	return "Failed pcall ?"
+end
+
+Library.EnumToString = function(KeyCode)
+	pcall(function()
+		local SplitString = string.split(tostring(KeyCode), ".")
+		local Name = SplitString[3]
+		return Name
+	end)
+	return "Failed pcall ?"
+end
+
+Library.Draggable = function(DragPoint, ToMove)
+	pcall(function()
+		local Dragging, DragInput, MousePos, FramePos = nil, nil, nil, nil
+		DragPoint.InputBegan:Connect(function(Input)
+			if Input.UserInputType == Enum.UserInputType.MouseButton1 then
+				Dragging = true
+				MousePos = Input.Position
+				FramePos = ToMove.Position
+
+				Input.Changed:Connect(function()
+					if Input.UserInputState == Enum.UserInputState.End then
+						Dragging = false
+					end
+				end)
+			end
+		end)
+		DragPoint.InputChanged:Connect(function(Input)
+			if Input.UserInputType == Enum.UserInputType.MouseMovement then
+				DragInput = Input
+			end
+		end)
+		UserInputService.InputChanged:Connect(function(Input)
+			if Input == DragInput and Dragging then
+				local Delta = Input.Position - MousePos
+				Library.Tween(ToMove, "Position", UDim2.new(FramePos.X.Scale,FramePos.X.Offset + Delta.X, FramePos.Y.Scale, FramePos.Y.Offset + Delta.Y), "Out", "Quint", 0.15)
+			end
+		end)
+	end)
+end
+
+Library.Click = function(Table)
+	local Click = Library.Create("TextButton", Table[1], {
+		BackgroundTransparency = 1,
+		Text = "",
+		Size = UDim2.new(1,0,1,0),
+		AutoButtonColor = false,
+	})
+	Click.Activated:Connect(function()
+		if Table.Callback then
+			return Table.Callback()
+		end
+	end)
+	return Click
 end
 
 return Library
+
